21687241983ef66201abf4daf497e39065e58125
tonny187 <tonyoanthony@gmail.com>
2023-11-24 19:04:55 -0500

    first commit in PHP

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//echo "Holaa esto esta hecho con php \n";
+
+$nombre = "Anthony";
+$apellido = "Quintero";
+
+//echo "Yo me llamo o my name is " . $nombre . " " . $apellido . "\n";
+
+//echo "no, no. Yo me llamo $nombre $apellido \n";
+
+//echo "El resultado de 4 x 5 es " . (4 * 5) . "\n";
+
+$personas = [
+    "Tonny" => 32,
+    "Mae" => 40,
+    "Denys" => 17,
+    "Thomas" => 20
+];
+
+//var_dump( $personas );
+print_r( $personas );
+
+echo "\n";
