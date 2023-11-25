1a9712c019b577e4fcbb50c2b7707735c97f354b
tonny187 <tonyoanthony@gmail.com>
2023-11-24 19:34:07 -0500

    adding other directory

---
+++ variables/variables.php
@@ -0,0 +1,12 @@
+<?php
+
+// variables
+$numero_1 = 8;
+$numero_2 = 7;
+
+// Constante
+define("NUMERO_PI", 3.14);
+
+echo "El resultado de la suma es: " . ($numero_1 + $numero_2) . "\n";
+
+echo NUMERO_PI . "\n";
