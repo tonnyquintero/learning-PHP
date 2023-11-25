63a2cc80aa58cd15496cb810d489eb2e4da5f7ef
tonny187 <tonyoanthony@gmail.com>
2023-11-25 16:20:19 -0500

    adding type of data

---
+++ casting.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$numerito = "5";
+
+//Aqui forzamos el tipo de dato en la variable
+$numerito = (int) $numerito;
+
+var_dump( $numerito ) . "\n";
+
+$dias = 5.89;
+
+//Aqui forzamos el tipo de dato en la variable
+$dias = (int) $dias;
+
+var_dump( $dias ) . "\n";
+
+$banderita = 1;
+
+//Aqui forzamos el tipo de dato en la variable
+$banderita = (bool) $banderita;
+
+var_dump( $banderita ) . "\n";
